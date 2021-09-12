35bbc9ca0824303d67eddb0637a369a72f647021
lijiajun <1243148239@qq.com>
2021-09-12 16:34:01 +0800

    提交所有文件

--- read_db.py
+++ read_db.py
@@ -1,22 +1,20 @@
 import pymysql
-#链接数据库
+
 db=pymysql.connect(host='localhost',
                    port=3306,
                    user='root',
                    password='123456',
                    database='stu',
                    charset='utf8')
-#获取游标（操作数据库，执行sql语句）
+
 cur=db.cursor()
 
-#sql语句
-sql="insert into class values (6,'张国荣',25,'男',95);"
+sql="select * from class where sex='男';"
 
-cur.execute(sql)#执行sql语句
+cur.execute(sql)
 
-db.commit()#将写操作提交，多次写一同提交
+get_data=cur.fetchall()
+print(get_data)
 
-#关闭数据库
 cur.close()
 db.close()
-
